c97c8c92348ce4cd7cadc22219cb44c82a4d2191
Sebastien Alaiwan <Unknown>
2017-04-25 07:45:57 +0200

    Automatic web archive creation + use an 'index.html'

--- update.sh
+++ update.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 set -euo pipefail
+
+readonly tmpDir=/tmp/ld38-naarrow-deliver-$$
+trap "rm -rf $tmpDir" EXIT
+mkdir -p $tmpDir
+
 ./check
+
+#------------------------------------------------------------------------------
+# create game directory
+#------------------------------------------------------------------------------
+readonly gameDir=$tmpDir/naarrow
+mkdir -p $gameDir
+
 rsync \
   --compress \
   --delete-excluded \
@@ -8,4 +20,23 @@ rsync \
   --exclude "*.bc" \
   --exclude "*.o" \
   --exclude "*.deps" \
-  -vr bin/asmjs/* alaiwans@code.alaiwan.org:public_html/naarrow
+  -vr bin/asmjs/* $gameDir
+cp index.html $gameDir/index.html
+
+#------------------------------------------------------------------------------
+# archive it
+#------------------------------------------------------------------------------
+pushd $tmpDir
+zip naarrow.zip -r naarrow
+popd
+
+mv $tmpDir/naarrow.zip .
+
+#------------------------------------------------------------------------------
+# upload it to code.alaiwan.org
+#------------------------------------------------------------------------------
+rsync \
+  --compress \
+  --delete \
+  -vr $gameDir/* alaiwans@code.alaiwan.org:public_html/games/naarrow
+
